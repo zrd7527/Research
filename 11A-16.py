bd2b4c2947634259a722074f6cc965faa004e15e
zrd7527 <zrd7527@rit.edu>
2020-06-11 10:36:34 -0400

    Thursday Morning Upload

--- 11A-16.py
+++ 11A-16.py
@@ -18,7 +18,10 @@ def start(filename):
     ar = p.Archive(filename)
     ar.dedisperse(reverse = True)
     rm = mitigate(ar)
-    ar.bscrunch(nbins = 2048, factor = 4)       # Average Burst in Phase
+    if filename == '12B_743sec.calib.4p':
+        ar.bscrunch(nbins = 3000, factor = 6)
+    else:
+        ar.bscrunch(nbins = 2048, factor = 4)       # Average Burst in Phase
     ar.fscrunch(nchan = 19456, factor = 304)    # Average Burst in Frequency
     data = ar.getData()
     fax = ar.getAxis(flag = 'F')        # Frequency Axis
@@ -60,11 +63,14 @@ def burst_prop(burst):
     SN = sp.getSN()
     return(sp, FWHM, SN)
 
-def freq_av(data):
+def freq_av(data, tag, plot, xlims):
     ''' 
         Simple average in frequency
         Inputs:
             data - Array of data arrays
+            tag - Burst Name, e.g. 11A
+            plot - Boolean, true to make plot
+            xlims - Tuple of phase range to zoom in on plot
         Returns:
             av - Array of flux values of burst averaged in frequency
     '''
@@ -77,6 +83,13 @@ def freq_av(data):
             tot += data[i]
             count += 1
     av = tot/count
+    if plot == True:
+        plt.plot(av)
+        plt.xlim(xlims[0], xlims[1])
+        plt.xlabel('Phase Bins')
+        plt.ylabel('Flux Density')
+        plt.title('Burst ' + tag + ' Averaged in Frequency')
+        plt.savefig(tag + '_FreqAv')
     return(av)
 
 def mitigate(ar):
@@ -184,7 +197,7 @@ def comp_param(data, mode, n, pllim, phlim, fllim, fhlim, fax, tag):
                 widths[1].append(x[1])                          # Giving extra noise around component provides more accurate FWHM
                 amps[1].append(GetFit[0][j])
                 mus[1].append(GetFit[1][j])
-                fluence[1].append(np.sum(GetFit[2][(pllim[1]-pllim[0]):(phlim[1]-pllim[0])])/(1000*2892))
+                fluence[1].append(np.sum(GetFit[2][(pllim[1]-pllim[0]):(phlim[1]-pllim[0])])/(1000*2892))   # All index ranges of fluence calculation offset by pllin[0] becasue that is where the fit starts
             elif pllim[2] < GetFit[1][j] < phlim[2] and (len(mus[2]) - 1) < i and fllim[2] < i < fhlim[2]:
                 x = burst_prop(data[i][(phlim[1]+1):pllim[3]])
                 widths[2].append(x[1])
@@ -236,6 +249,25 @@ def manual_gaussians(x, amp, mu, sigma):
             curve += u.gaussian(x = x, amp = amp[i], mu = mu[i], sigma = sigma[i])
     return(curve)
 
+def lognorm(x, amp, mu, sigma):
+    '''
+        Creates log-normal distribution with input variables
+        Inputs:
+            x - x axis
+            amp - Amplitude of distribution
+            mu - Center of distribution
+            sigma - Standard deviation of distribution
+        Returns:
+            array of log-normal curve values
+    '''
+    center = []
+    for i in range(0, len(x)):
+        if x[i]-mu == 0:
+            center.append(10e-10)
+        else:
+            center.append(np.log(x[i]-mu))
+    return amp*np.exp(-0.5*(center/sigma)**2)
+
 def fit(burst, mode, n, llimit, hlimit, freq, tag, plot):
     '''
         Fits n components to data array and can plot fit with burst for comparison
@@ -254,7 +286,7 @@ def fit(burst, mode, n, llimit, hlimit, freq, tag, plot):
             mu - Array of component centers
             retval - Gaussian curve array
     '''
-    x = np.linspace(start=llimit, stop=hlimit, num=(hlimit-llimit))
+    x = np.linspace(start = llimit, stop = hlimit, num = (hlimit-llimit))
     amp = []
     mu = []
     ForceFit = u.fit_components(xdata = x, ydata = burst[llimit:hlimit], mode = mode,  N=n)    # Forces a fit of n components
@@ -272,6 +304,31 @@ def fit(burst, mode, n, llimit, hlimit, freq, tag, plot):
         plt.title(tag + ' Peak Flux (at ' + str(round(freq)) + ' MHz)')
         plt.savefig(tag + '_Fit_Test')
     return(amp, mu, retval)
+
+def lnorm_fit(yin, n, xlow, xhigh):
+    xran = np.linspace(start = xlow, stop = xhigh, num = (xhigh-xlow))
+    nbins = len(xran)
+    peakind = np.argmax(yin)
+    init = np.array([yin[peakind], xran[peakind], nbins])
+    for k in range(1, n+1):
+        def findfit(p, x):
+            pdf = np.zeros(len(x))
+            for i in range(k):
+                if p[3*i] <= 0:
+                    return 1e12
+                pdf += u.gaussian(x, p[3*i], p[3*i+1], p[3*i+2])
+            return pdf
+        def err(p, x, y):
+            return y - findfit(p, x)
+        res = sci.optimize.leastsq(err, init, args = (xran, yin), full_output = True)
+        if k == n:
+            break
+        pfit = res[0]
+        resid = yin - findfit(pfit, xran)
+        peakind = np.argmax(resid)
+        newinit = np.array([resid[peakind], xran[peakind], nbins])
+        init = np.concatenate((pfit, newinit))
+    return res[0]
 
 def data_plot(data, fax, tag, center):
     ''' 
@@ -335,15 +392,17 @@ def burst_11A_prop():
             nothing
         Returns:
             params - Array of component parameters for burst 11A
+            fax - Frequency axis array
     '''
     new = start(filename = '11A_16sec.calib.4p')
     tag = '11A'
+    fax = new[2]
     PhaseLowLims = [350, 363, 380, 395]
     PhaseHighLims = [362, 370, 390, 420]
     FreqLowLims = [11, 10, 17, 33]
     FreqHighLims = [26, 35, 47, 52]
     params = comp_param(data = new[1], mode = 'gaussian', n = 4, pllim = PhaseLowLims, phlim = PhaseHighLims, fllim = FreqLowLims, fhlim = FreqHighLims, fax = new[2], tag = tag)
-    return(params)
+    return(params, fax)
 
 def burst_12B_prop():
     '''
@@ -352,44 +411,38 @@ def burst_12B_prop():
             nothing
         Returns:
             params - Array of component parameters for burst 12B
-            offset - Center of components array with offset removed
+            fax - Frequncy axis array
     '''
     new = start(filename = '12B_743sec.calib.4p')
     tag = '12B'
-    PhaseLowLims = [93, 125, 160, 0]            # Must include a third lower limit for width calculation
-    PhaseHighLims = [113, 150, 0, 0]
+    fax = new[2]
+    PhaseLowLims = [65, 85, 115, 0]            # Must include a third lower limit for width calculation
+    PhaseHighLims = [75, 105, 0, 0]
     FreqLowLims = [ 11, 28, 0, 0]
     FreqHighLims = [25, 45, 0, 0]
     params = comp_param(data = new[1], mode = 'gaussian', n = 2, pllim = PhaseLowLims, phlim = PhaseHighLims, fllim = FreqLowLims, fhlim = FreqHighLims, fax = new[2], tag = tag)
-    offset = [[], []]
-    for i in params[1][0]:
-        real = i-32
-        offset[0].append(real)
-    for i in params[1][1]:
-        real = i-45
-        offset[1].append(real)
-    return(params, offset)
+    return(params, fax)
 
 def main():
     x = np.linspace(0, 64, 64)
-    lnorm = sci.stats.lognorm.pdf(x, s=0.25)
-    plt.plot(lnorm)
-    plt.savefig('TEST')
-    #params = burst_11A_prop()
-    #for i in params[3]:
-        #plt.plot(i)
-    '''
-    newav = freq_av(data = new[1][10:45])
-    plt.plot(newav)
-    plt.xlim(50, 200)
+    props = burst_11A_prop()
+    params = props[0]
+    fax = props[1]
+    fluence1 = params[3][1]
+    pfit = lnorm_fit(yin = fluence1, n = 1, xlow = 0, xhigh = 64)
+    #pdf = np.zeros(len(x))
+    #pdf += lognorm(x, pfit[0], pfit[1], pfit[2])
+    lnorm = pfit[0]*(sci.stats.lognorm.pdf(x = x, s = pfit[2], loc = pfit[1], scale = 0.5))
+    plt.plot(fax, fluence1)
+    plt.plot(fax, lnorm)
     plt.xlabel('Phase Bins')
-    plt.ylabel('Flux Density')
-    plt.title('Burst 12B Averaged in Frequency')
-    plt.savefig('12B_FreqAv')
-    '''
+    plt.ylabel('Fluence(Jy ms)')
+    plt.title('Log-Normal Fit to Burst 11A Fluence')
+    plt.savefig('11A_Fluence_Fit')
+    
     #fit(burst = params[3][3], mode = 'gaussian', n = 1, llimit = 30, hlimit = 64, freq = 6000, tag = '11A', plot = True)
     #labels = ('Comp 1', 'Comp 2', 'Comp 3', 'Comp 4')
-    #data_plot(data = new[1], fax = new[2], tag = tag, center = offset)
-    #comp_plot(data = params[3][0:2], name = 'Fluence', fax = new[2], units = 'Jy ms', tag = tag, labels = labels, log = False)
+    #data_plot(data = props[1], fax = props[2], tag = tag, center = props[0][1])
+    #comp_plot(data = params[3][0:2], name = 'Fluence', fax = props[1], units = 'Jy ms', tag = tag, labels = labels, log = False)
 
 main()
