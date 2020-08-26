d9f36c4e614284283d222110f0ab2996de6dd2db
zrd7527 <zrd7527@rit.edu>
2020-08-26 12:59:17 -0400

    Wednesday Upload

--- ExtendedData.py
+++ ExtendedData.py
@@ -3,9 +3,20 @@ import matplotlib.pyplot as plt
 from blimpy import Waterfall
 from scipy.signal import detrend
 
-def data_plot():
-    fb = Waterfall('spliced_guppi_57991_51723_DIAG_FRB121102_0012.gpuspec.0001.8.fil', t_start = 0, t_stop = 1000)
-    fb.info()
+def load(filename, info, tstart, tstop):
+    '''
+        Loads file data as a waterfall plot type array and can print data info
+        Inputs:
+            filename - name of file being loaded
+            info - Boolean, True to print data info
+            tstart - Starting time bin
+            tstop - Ending time bin
+        Returns:
+            cleandata - Detrended (bandpass removed) array of data arrays
+    '''
+    fb = Waterfall(filename, t_start = tstart, t_stop = tstop)
+    if info == True:
+        fb.info()
     freqs, data = fb.grab_data(4000, 8000, 1)
     newdat = []
     for i in range(0, len(data[0])):
@@ -13,10 +24,25 @@ def data_plot():
         for j in range(0, len(data)):
             newarr.append(data[j][i])
         newdat.append(newarr)
-    clean = detrend(newdat)
-    plt.imshow(clean, origin = 'lower', interpolation = 'nearest', aspect = 'auto')
-    plt.savefig('DetrendedData_51723')
+    cleandata = detrend(newdat)
+    return(cleandata)
+
+def data_plot(data, name, tag):
+    '''
+        Makes waterfall plot of input data
+        Inputs:
+            data - Array of data arrays
+            name - save name
+            tag - save tag
+        Returns:
+            nothing
+    '''
+    plt.imshow(data, origin = 'lower', interpolation = 'nearest', aspect = 'auto')
+    plt.ylabel('Frequency Bins')
+    plt.xlabel('Time Bins')
+    plt.savefig(name + '_' + tag)
 
 def main():
-    data_plot()
+    data1 = load(filename = 'spliced_guppi_57991_51723_DIAG_FRB121102_0012.gpuspec.0001.8.fil', info = False, tstart = 894000, tstop = 895000)
+    data_plot(data = data1, name = 'BurstSearch', tag = '51723')
 main()
