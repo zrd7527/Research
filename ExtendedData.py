2ec861410d851808bc2c84c5d018018244579fed
zrd7527 <zrd7527@rit.edu>
2020-09-15 12:16:30 -0400

    Dedispersion Algorithm Created

--- ExtendedData.py
+++ ExtendedData.py
@@ -27,6 +27,20 @@ def load(filename, info, tstart, tstop):
     cleandata = detrend(newdat)
     return(cleandata)
 
+def get_freqs(fch1, nchan, foff):
+    return(fch1 + np.arange(nchan)*foff)
+
+def dedisperse(data, dm, freqs, tsamp):
+    delay_bins = []
+    dedispersed = []
+    for i in range(0, len(freqs)):
+        delay_time = 4148808.0 * dm * (1/(freqs[0]**2) - (1/(freqs[i]**2)))/1000
+        delay_bins.append(int(np.round(delay_time/tsamp)))
+        dedispersed.append(np.zeros(len(data[0]), dtype = np.float32))
+    for j in range(0, len(data)):
+        dedispersed[j] = np.concatenate([data[j][-delay_bins[j]:], data[j][:-delay_bins[j]]])
+    return(dedispersed)
+
 def data_plot(data, name, tag):
     '''
         Makes waterfall plot of input data
@@ -43,6 +57,8 @@ def data_plot(data, name, tag):
     plt.savefig(name + '_' + tag)
 
 def main():
-    data1 = load(filename = 'spliced_guppi_57991_51723_DIAG_FRB121102_0012.gpuspec.0001.8.fil', info = False, tstart = 894000, tstop = 895000)
-    data_plot(data = data1, name = 'BurstSearch', tag = '51723')
+    data1 = load(filename = 'spliced_guppi_57991_49905_DIAG_FRB121102_0011.gpuspec.0001.8.fil', info = False, tstart = 46000, tstop = 47000)
+    freqs = get_freqs(fch1 = 9313.78173828125, nchan = 14848, foff = -0.3662109375)
+    dedisdata = dedisperse(data = data1, dm = 558, freqs = freqs, tsamp = 0.0003495253333333333)
+    data_plot(data = dedisdata, name = 'Dedispersed', tag = '11A')
 main()
